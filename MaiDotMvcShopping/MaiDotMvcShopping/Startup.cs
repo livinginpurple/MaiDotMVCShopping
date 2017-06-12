f8dcad5299b7fb6bcf43bc9feeb96f054b97d6fd
Roger Chen <kyrc0205@gmail.com>
2017-06-12 09:38:35 +0800

    MVC 原始範本建立

---
+++ MaiDotMvcShopping/MaiDotMvcShopping/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MaiDotMvcShopping.Startup))]
+namespace MaiDotMvcShopping
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
