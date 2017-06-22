00b1abe0938454852a366a57ddf061c8613448d2
Roger Chen <kyrc0205@gmail.com>
2017-06-22 17:19:48 +0800

    day 15 - 會員管理後台 修改 Index - 判斷是否有訊息存在 修改 Edit - 修改顯示文字 修改 ManageUserController - Post 後後儲存修改之資料，及錯誤訊息傳遞。

--- MaiDotMvcShopping/MaiDotMvcShopping/Controllers/ManageUserController.cs
+++ MaiDotMvcShopping/MaiDotMvcShopping/Controllers/ManageUserController.cs
@@ -14,6 +14,7 @@ namespace MaiDotMvcShopping.Controllers
             ViewBag.ResultMessage = TempData["ResultMessage"];
             using (Models.UserEntities db = new Models.UserEntities())
             {
+                // 取得所有 AspNetUsers 的資料，並放入 Models.ManageUser 模式中
                 var result = db.AspNetUsers.Select(
                     s => new Models.ManageUser
                     {
@@ -43,6 +44,7 @@ namespace MaiDotMvcShopping.Controllers
                 }
             }
 
+            // 設定錯誤訊息
             TempData["ResultMessage"] = $"使用者 {id} 不存在，請重新操作。";
             return RedirectToAction("Index");
         }
@@ -50,7 +52,28 @@ namespace MaiDotMvcShopping.Controllers
         [HttpPost]
         public ActionResult Edit(Models.ManageUser model)
         {
-            return View();
+            using (Models.UserEntities db = new Models.UserEntities())
+            {
+                var result = (from s in db.AspNetUsers
+                              where s.Id == model.Id
+                              select s).FirstOrDefault();
+
+                if (result != default(Models.AspNetUsers))
+                {
+                    result.UserName = model.UserName;
+                    result.Email = model.Email;
+
+                    db.SaveChanges();
+
+                    // 設定成功訊息
+                    TempData["ResultMessage"] = $"使用者 {model.UserName} 成功編輯。";
+                    return RedirectToAction("Index");
+                }
+            }
+
+            // 設定錯誤訊息
+            TempData["ResultMessage"] = $"使用者 {model.UserName} 不存在，請重新操作。";
+            return RedirectToAction("Index");
         }
     }
 }
