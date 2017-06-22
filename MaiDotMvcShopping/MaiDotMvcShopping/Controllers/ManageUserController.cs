f1226566544f8a35ecc61eb2d2fce45a18dfe718
Roger Chen <kyrc0205@gmail.com>
2017-06-22 16:41:39 +0800

    修改 _Layout - _LoginPartial 路徑錯誤

--- MaiDotMvcShopping/MaiDotMvcShopping/Controllers/ManageUserController.cs
+++ MaiDotMvcShopping/MaiDotMvcShopping/Controllers/ManageUserController.cs
@@ -15,14 +15,36 @@ namespace MaiDotMvcShopping.Controllers
             using (Models.UserEntities db = new Models.UserEntities())
             {
                 var result = db.AspNetUsers.Select(
-                    s => new Models.ManageUser { Id = s.Id, UserName = s.UserName, Email = s.Email });
+                    s => new Models.ManageUser
+                    {
+                        Id = s.Id,
+                        UserName = s.UserName,
+                        Email = s.Email
+                    });
                 return View(result.ToList());
             }
         }
 
         public ActionResult Edit(string id)
         {
-            return View();
+            using (Models.UserEntities db = new Models.UserEntities())
+            {
+                var result = db.AspNetUsers.Where(w => w.Id == id)
+                    .Select(s => new Models.ManageUser
+                    {
+                        Id = s.Id,
+                        UserName = s.UserName,
+                        Email = s.Email
+                    }).FirstOrDefault();
+
+                if (result != default(Models.ManageUser))
+                {
+                    return View(result);
+                }
+            }
+
+            TempData["ResultMessage"] = $"使用者 {id} 不存在，請重新操作。";
+            return RedirectToAction("Index");
         }
 
         public ActionResult Edit(Models.ManageUser model)
