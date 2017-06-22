3086cbd918115aec80e7d5468448fc55888a76be
Roger Chen <kyrc0205@gmail.com>
2017-06-22 15:35:54 +0800

    day 15 - 會員管理後台 新增 ManageUserController - 加入 Index, Edit ActionResult，實作 Index取得 AspNetUser 資料。 新增 View/Index - 顯示 AspNetUser 資料

---
+++ MaiDotMvcShopping/MaiDotMvcShopping/Controllers/ManageUserController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MaiDotMvcShopping.Controllers
+{
+    public class ManageUserController : Controller
+    {
+        // GET: ManageUser
+        public ActionResult Index()
+        {
+            ViewBag.ResultMessage = TempData["ResultMessage"];
+            using (Models.UserEntities db = new Models.UserEntities())
+            {
+                var result = db.AspNetUsers.Select(
+                    s => new Models.ManageUser { Id = s.Id, UserName = s.UserName, Email = s.Email });
+                return View(result.ToList());
+            }
+        }
+
+        public ActionResult Edit(string id)
+        {
+            return View();
+        }
+
+        public ActionResult Edit(Models.ManageUser model)
+        {
+            return View();
+        }
+    }
+}
