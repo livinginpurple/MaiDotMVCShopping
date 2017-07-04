11e9caa48d8a4cf7dc6d059f9497fde535416ce4
Roger Chen <kyrc0205@gmail.com>
2017-07-04 17:34:31 +0800

    day 26 - 後台訂單列表功能 新增 Details - 根據 id 傳入所有商品列表 修改 Index - 取得訂單所有資料

--- MaiDotMvcShopping/MaiDotMvcShopping/Controllers/ManageOrderController.cs
+++ MaiDotMvcShopping/MaiDotMvcShopping/Controllers/ManageOrderController.cs
@@ -11,7 +11,33 @@ namespace MaiDotMvcShopping.Controllers
         // GET: ManageOrder
         public ActionResult Index()
         {
-            return View();
+            using (Models.CartsEntities db = new Models.CartsEntities())
+            {
+                // 取得 Order 中所有資料。
+                var result = db.Orders.Select(s => s).ToList();
+                return View(result);
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            using (Models.CartsEntities db = new Models.CartsEntities())
+            {
+                // 取得 OrderId 為傳入 id 的所有商品列表
+                var result = db.OrderDetails
+                    .Where(w => w.OrderId == id)
+                    .Select(s => s).ToList();
+
+                if (result.Any())
+                {
+                    return View(result);
+                }
+                else
+                {
+                    // 如果商品數目為零，代表該訂單異常 (無商品)。導回商品列表。
+                    return RedirectToAction("Index");
+                }
+            }
         }
     }
 }
