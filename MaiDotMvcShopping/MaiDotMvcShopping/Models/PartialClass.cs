03c1b466f4845f1a2e26c2b156b37d8ab443dce3
Roger Chen <kyrc0205@gmail.com>
2017-07-05 17:18:32 +0800

    day 27 - 使用者[我的訂單]功能 新增 PartialClass - 加入 GetUserName 方法

---
+++ MaiDotMvcShopping/MaiDotMvcShopping/Models/PartialClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MaiDotMvcShopping.Models
+{
+    public class PartialClass
+    {
+    }
+
+    /// <summary>
+    /// 定義 Models.Order 的部分類別
+    /// </summary>
+    public partial class Order
+    {
+        /// <summary>
+        /// 取得訂單中的使用者暱稱
+        /// </summary>
+        /// <returns>回傳 使用者暱稱</returns>
+        public string GetUserName()
+        {
+            using (Models.UserEntities db = new UserEntities())
+            {
+                var result = db.AspNetUsers
+                    .Where(w => w.Id == this.UserId)
+                    .Select(s => s.UserName).FirstOrDefault();
+
+                // 回傳找到的 UserName
+                return result;
+            }
+        }
+    }
+}
